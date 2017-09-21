e3fe7da8b6221d99258ae742bd67d44d30363f88
bwegener <wegener_brian@yahoo.com>
2017-09-21 00:19:26 -0700

    Finished OC Cars

--- app/src/main/java/com/example/bwegener/occars/CarLoan.java
+++ app/src/main/java/com/example/bwegener/occars/CarLoan.java
@@ -50,69 +50,102 @@ public class CarLoan {
     /**
      * This sets the down payment, from what the user has entered.
      *
-     * @param downPayment the downpayment for the car.
+     * @param downPayment the down payment for the car.
      */
     public void setDownPayment(double downPayment)
     {
         mDownPayment = downPayment;
     }
 
+    /**
+     * This gets the term for the car loan.
+     *
+     * @return the term of the car loan.
+     */
     public double getTerm()
     {
         return mTerm;
     }
 
+    /**
+     * This sets the term for the car loan from what the user has chosen.
+     *
+     * @param term the term for the car loan.
+     */
     public void setTerm(int term)
     {
         mTerm = term;
     }
 
-
+    /**
+     * This finds the tax amount for the car.
+     *
+     * @return the tax amount.
+     */
     public double taxAmount()
     {
         return mPrice * STATE_TAX;
     }
 
+    /**
+     * This gets the total amount for the car.
+     *
+     * @return the total amount for the car.
+     */
     public double totalAmount()
     {
         return mPrice + taxAmount();
     }
 
+    /**
+     * This gets the borrowed amount for the car.
+     *
+     * @return the borrowed amount for the car.
+     */
     public double borrowedAmount()
     {
-        return mPrice - mDownPayment;
+        return totalAmount() - mDownPayment;
     }
 
+    /**
+     * This gets the interest amount for the car.
+     *
+     * @return the interest amount for the car.
+     */
     public double interestAmount()
     {
-        /*
-        3 year APR = 4.62%
-        4 year APR = 4.19%
-        5 year APR = 4.16%
-         */
-        double interestRate;
-        switch(mTerm)
-        {
-            case 3:
-                interestRate = 0.0462;
-                break;
-            case 4:
-                interestRate = 0.0419;
-                break;
-            case 5:
-                interestRate = 0.0416;
-                break;
-            default:
-                // Should never be used
-                interestRate = 0.10;
-                break;
-        }
-        return borrowedAmount() * interestRate;
+        return mTerm * 12 * monthlyPayment() - borrowedAmount();
     }
 
+    /**
+     * This gets the monthly payment for the car.
+     *
+     * @return the monthly payment for the car.
+     */
     public double monthlyPayment()
     {
-        return (borrowedAmount() + interestAmount()) / (mTerm * 12);
+        double monthlyInterestRate = 0.0;
+
+        switch (mTerm)
+        {
+            case 3:
+                monthlyInterestRate = 0.0462 / 12;
+                break;
+            case 4:
+                monthlyInterestRate = 0.0419 / 12;
+                break;
+            case 5:
+                monthlyInterestRate = 0.0416 / 12;
+                break;
+            default:
+                monthlyInterestRate = 10;
+                break;
+                // This should not happen
+        }
+
+        double interestExpression = Math.pow(1 + monthlyInterestRate, mTerm * 12.0);
+
+        return borrowedAmount() * monthlyInterestRate * interestExpression / (interestExpression - 1);
     }
 
 }
